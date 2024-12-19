daffd061a46ca3937d5225681401d709a0d246d8
Tumensetit <124162824+Tumensetit@users.noreply.github.com>
2024-12-20 00:33:10 +0100

    Update task1_2.py
    testing someone elses WORKING file

--- task1_2.py
+++ task1_2.py
@@ -1,43 +1,32 @@
-import math
-from flask import Flask, request, jsonify
 import os
+
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
 
 app = Flask(__name__)
 
-def numerical_integration(lower, upper, n):
-    totalArea = 0
-    width = (upper - lower) / n
 
-    for i in range(n):
-        x = lower + i * width
-        height = abs(math.sin(x))
-        totalArea += height * width
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
 
-    return totalArea
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/test')
-def test():
-    return 'this is a test'
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
 
-@app.route('/<lower>/<upper>', methods=['GET'])
-def get_integral(lower, upper):
-    lower = float(lower)
-    upper = float(upper)
-    nValues = [10, 100, 1000, 10000, 100000, 1000000]
-    results = []
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
 
-    for n in nValues:
-        result = numerical_integration(lower, upper, n)
-        results.append({
-            'n': n,
-            'result': result
-        })
-    
-    return jsonify({
-        'lower': lower,
-        'upper': upper,
-        'results': results
-    })
 
 if __name__ == '__main__':
-       app.run(debug=True, host='0.0.0.0')
+   app.run()
