1f46ca04505997f1e0d7c115fce2b179f613d2ba
Tumensetit <124162824+Tumensetit@users.noreply.github.com>
2024-12-19 21:47:07 +0100

    Update task1_2.py

--- task1_2.py
+++ task1_2.py
@@ -1,5 +1,6 @@
 import math
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -39,4 +40,5 @@ def get_integral(lower, upper):
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+       port = int(os.environ.get('PORT', 5000))
+       app.run(host='0.0.0.0', port=port)
